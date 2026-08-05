a1d917292ebe55c08e22b1d3959e79a6d4715667
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-08-05 23:40:27 +0300

    restruct_v0.0.5
    separate the main space to avoid conflicts.

--- server/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-export const authRoutes = (fastify, options, done) => {
-    const { authController } = options;
-    
-    fastify.post('/api/auth/register', authController.register);
-    fastify.post('/api/auth/login', authController.login);
-    
-    done();
-};
